74117684ca23df1a44e2c75d0849d7af59c40e50
renout <frederic.renout@agipi.com>
2023-02-28 07:34:57 +0100

    divers fix

--- TD3/src/main/java/fr/unistra/l2/poo2/rle/Compresseur.java
+++ TD3/src/main/java/fr/unistra/l2/poo2/rle/Compresseur.java
@@ -3,10 +3,7 @@ package fr.unistra.l2.poo2.rle;
 import lombok.RequiredArgsConstructor;
 
 import java.nio.ByteBuffer;
-import java.util.Collection;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 import static fr.unistra.l2.poo2.rle.RLE.CTRL_POIDS_FORT;
 
@@ -14,6 +11,13 @@ import static fr.unistra.l2.poo2.rle.RLE.CTRL_POIDS_FORT;
 public class Compresseur {
 
     private final ColorConverter converter;
+
+    private static byte[] shortToArray(short nbRepetitions, boolean compresse) {
+        ByteBuffer buffer = ByteBuffer.allocate(2);
+        short toput = compresse ? (short) (nbRepetitions | CTRL_POIDS_FORT) : nbRepetitions;
+        buffer.putShort(toput);
+        return buffer.array();
+    }
 
     public Queue<Byte> compresser(List<Color> image) {
         //la queue qui contiendra le resultat de la compression
@@ -31,20 +35,25 @@ public class Compresseur {
             if (nbRepetitions == 0) {
                 couleurRepetee = pixel;
                 nbRepetitions++;
-            } else if (couleurRepetee == pixel) {
+            } else if (Objects.equals(couleurRepetee, pixel)) {
                 // Si on est dans la répétition
                 nbRepetitions++;
                 //on "démarre" la compression
                 if (nbRepetitions == 3) {
                     //nb represente le nombre de pixels qui auraient pu etre bufferises avant qu'on compresse
-                    short nb = (short) (buffer.size() - 2); //-2 car en l33 on a incrementé le compteur (on regarde en fait juste si on a dans notre buffer autre chose que le pixel courant)
+                    var nb = buffer.size() - 2;
+                    //le buffer ne contient pas encore le pixel courant.
+                    // Si on a plus de 2 elements dans le buffer c'est qu'on a stocké en début de buffer
+                    // des elements qui ne sont pas de la couleur actuelle
                     if (nb != 0) {
-                        //par exemple si on a  BNBNBBBB -> dans le buffer on aura [B,N,B,N,B,B,B] => on doit "sauver"  [B,N,B,N] qui sont des pixels non compressé
+                        //par exemple si on a en buffer BNBNBB et qu'on recoit B
+                        // -> on doit "sauver" [B,N,B,N] qui sont des pixels non compressé
                         //avant de continuer à "stocker" la suite de notre buffer
-
-                        //on stocke les octets de controles = le nb de pixels non compressés qui vont etre inserés
-                        stockePixelsNonCompresses(nb, result, buffer);
+                        //les octets de controles = le nb de pixels non compressés qui vont etre inserés
+                        stockePixelsNonCompresses((short) nb, result, buffer);
                     }
+                    //sinon c'est qu'on est en train de stocker des pixels à compresser donc pas de traitement spécifique,
+                    // on rajoutera en fin de boucle le pixel courant
                 }
             } else if (nbRepetitions > 2) { //on a changé de couleur et on etait dans une repetition induisant une compression et on change par ex on a lu 4 B et on lit N
                 stockePixelCompresses(nbRepetitions, couleurRepetee, result);
@@ -71,21 +80,14 @@ public class Compresseur {
         //et le pixel qui est repetée
         List<Byte> couleurString = converter.toHexa(couleurRepetee);
         //on rajoute à notre chaine les octets de controle et le pixel repeté
-        final var array = shortToArray(nbRepetitions,true);
+        final var array = shortToArray(nbRepetitions, true);
         result.add(array[0]);
         result.add(array[1]);
         result.addAll(couleurString);
     }
 
-    private static byte[] shortToArray(short nbRepetitions, boolean compresse) {
-        ByteBuffer buffer = ByteBuffer.allocate(2);
-        short toput = compresse?(short) (nbRepetitions | CTRL_POIDS_FORT):nbRepetitions;
-        buffer.putShort(toput);
-        return buffer.array();
-    }
-
     private void stockePixelsNonCompresses(short nb, Queue<Byte> result, Queue<Color> buffer) {
-        final var array = shortToArray(nb,false);
+        final var array = shortToArray(nb, false);
         result.add(array[0]);
         result.add(array[1]);
         final var uncompressedList = buffer.stream().limit(nb).map(converter::toHexa).flatMap(Collection::stream).toList();
@@ -95,9 +97,9 @@ public class Compresseur {
     private void traiterBufferEnFinDeLecture(Queue<Byte> result, Queue<Color> buffer, Color couleurRepetee, short nbRepetitions) {
         //soit on a moins de 3 repet et on va stocket ce qu'on a dans le buffer sans compression
         if (nbRepetitions < 3) {
-            short nb = (short) buffer.size();
+            var nb = buffer.size();
             if (nb != 0) {
-                stockePixelsNonCompresses(nb, result, buffer);
+                stockePixelsNonCompresses((short) nb, result, buffer);
             }
             //soit on a 3 repets ou plus et la on a un pixel "compressé" dans notre buffer donc on le stocke
         } else {
